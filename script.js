02e2f7ee72eac1e881efdc453b295e0f078ca629
arab-game <126677212+arab-game@users.noreply.github.com>
2023-05-03 14:41:10 +0100

    arab game
    arab game

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 "use strict";
 document.addEventListener('contextmenu', event => event.preventDefault());
+
+// Disable right-click
+document.addEventListener('contextmenu', (e) => e.preventDefault());
+
+function ctrlShiftKey(e, keyCode) {
+  return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+}
+
+document.onkeydown = (e) => {
+  // Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
+  if (
+    event.keyCode === 123 ||
+    ctrlShiftKey(e, 'I') ||
+    ctrlShiftKey(e, 'J') ||
+    ctrlShiftKey(e, 'C') ||
+    (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
+  )
+    return false;
+};
+
 var UserStatus;
 (function (UserStatus) {
     UserStatus["LoggedIn"] = "Logged In";
